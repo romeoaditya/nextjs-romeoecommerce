cf6bece000bb7457ff239c2bec0466ae26a85a3b
romeoaditya <romeoaditya22@gmail.com>
2025-06-21 19:24:30 +0700

    feat: login with google

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -66,6 +66,17 @@ const LoginView = () => {
             {isLoading ? "Loading..." : "Login"}
           </button>
         </form>
+        <hr className={styles.login__form__divider} />
+        <div className={styles.login__form__other}>
+          <button
+            type="button"
+            onClick={() => signIn("google", {callbackUrl, redirect: false})}
+            className={styles.login__form__other__button}
+          >
+            <i className="bx bxl-google" />
+            Login With Google
+          </button>
+        </div>
       </div>
       <p className={styles.login__link}>
         Don't have an account? <Link href="/auth/register">Sign up here</Link>{" "}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import {SessionProvider} from "next-auth/react";
 import type {AppProps} from "next/app";
 import {Lato} from "next/font/google";
+import Head from "next/head";
 const lato = Lato({
   subsets: ["latin"],
   weight: ["100", "300", "400", "700", "900"],
@@ -13,6 +14,12 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <div className={lato.className}>
         <Navbar />
         <Component {...pageProps} />
