5efb80745a879a33e5317d614507492b9a134733
romeoaditya <romeoaditya22@gmail.com>
2025-06-21 17:16:45 +0700

    feat: register

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import type {AppProps} from "next/app";
+import {Lato} from "next/font/google";
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <div className={lato.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,10 @@
+import RegisterView from "@/components/views/auth/Register";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <RegisterView />
+    </>
+  );
+};
+export default RegisterPage;
