6ae5dcf0b16453fe2ca6d89d648ca873fa4fa894
romeoaditya <romeoaditya22@gmail.com>
2025-06-21 18:51:36 +0700

    test fix eslint

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({token, account, profile, user}: any) {
+    async jwt({token, account, user}: any) {
       if (account?.provider === "credentials") {
         token.email = user.email;
         token.fullname = user.fullname;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import {Geist, Geist_Mono} from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import {useSession} from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,8 +14,6 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
-  const {data} = useSession();
-
   return (
     <>
       <Head>
